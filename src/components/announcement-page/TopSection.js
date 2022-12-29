214eea0921ce94b956e768acfd06d4b960ce263f
ouassim-hamdani <ouassim5gamers@gmail.com>
2022-12-30 00:25:00 +0100

    Implemented top section of announce page

--- src/components/announcement-page/TopSection.js
+++ src/components/announcement-page/TopSection.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import Typography from '@mui/material/Typography';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import { useState } from 'react';
+import { imagesAnnounce } from '../../constants/constants';
+export const TopSection = () => {
+  let [slidesShow,setSlidesShow] = useState(3);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
 
-const TopSection = () => {
   return (
-    <div>TopSection</div>
+    <div className='p-10 space-y-4'>
+      <Typography variant="h5" className='font-semibold text-gray-900'>Full math revision course 2022</Typography>
+      <div className='rounded-xl px-4 pb-10 pt-3 space-y-3 border border-gray-200 '>
+        <Typography variant="h6" className='text-green-600'>1500 DA</Typography>
+        <Slider {...settings} className="">
+          {imagesAnnounce.map((item)=>{
+            return(
+              <img key={item.id} className='rounded-xl px-1'  src={item.img} alt="" />
+            );
+
+          })}
+        </Slider>
+      </div>
+      
+    </div>
   )
 }
-
-export default TopSection
