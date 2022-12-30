e8f405989939c15c4fc5d5363c535cc68c7635a9
ouassim-hamdani <ouassim5gamers@gmail.com>
2022-12-30 01:04:13 +0100

    Implemented profile section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { SearchSection } from './components/search-page/SearchSection';
+import { ProfileSection } from './components/profile-page/ProfileSection';
 import { createTheme, ThemeProvider} from "@mui/material/styles";
 function App() {
   const theme = createTheme({
@@ -10,7 +10,7 @@ function App() {
   });
   return (
     <ThemeProvider theme={theme}>
-      <SearchSection/>
+      <ProfileSection/>
     </ThemeProvider>
     
   );
